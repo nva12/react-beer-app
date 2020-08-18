3093a4e50c4226707fc96225cd050729f756626e
nva12 <nvallee.a7@gmail.com>
2020-08-18 15:18:06 +0400

    add media query to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,23 +3,45 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 // Styles
 import { makeStyles } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     marginBottom: theme.spacing(3),
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
   },
 }));
 
 const Header = () => {
   const classes = useStyles();
+  const matches = useMediaQuery('(min-width:600px');
 
   return (
     <header className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6">React Beer App</Typography>
+          <Typography variant="h6" className={classes.title}>
+            React Beer App
+          </Typography>
+          {matches ? (
+            <nav>
+              <Button color="inherit">Products</Button>
+              <Button color="inherit">Favorites</Button>
+              <Button color="inherit">About</Button>
+            </nav>
+          ) : (
+            <IconButton color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+          )}
         </Toolbar>
       </AppBar>
     </header>
