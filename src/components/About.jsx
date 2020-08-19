9a876cc52a33cecb3e09786fad49beff661d8c87
nva12 <nvallee.a7@gmail.com>
2020-08-19 11:52:20 +0400

    write about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,31 @@ const About = () => {
       <Card>
         <CardContent>
           <Typography variant="body1" gutterBottom>
-            This Web App is built with React, using React Hooks.
+            Hello, my name is Nicolas Vallée, I am a web developer specialized
+            in React.
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            This app is a personal front-end practice project built with
+            Create-React-App, and using React Hooks. It relies on the{' '}
+            <a href="https://punkapi.com/">PunkAPI</a> to fetch product data
+            about beers.
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            You can filter the list of beers by name and by alcohol content by
+            volume (ABV), and add a product to your list of favorites by
+            clicking on the star icon.
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            I used <a href="https://material-ui.com/">Material-UI</a>{' '}
+            framework’s components to design the user interface.
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            You can find the source code in this{' '}
+            <a href="https://github.com/nva12/react-beer-app">GitHub repo</a>.
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            See my other projects and find more information about me{' '}
+            <a href="https://www.nicolasvallee.dev/">on my portfolio</a>.
           </Typography>
           <Typography variant="body1" gutterBottom>
             &copy; 2020 Nicolas Vallée
